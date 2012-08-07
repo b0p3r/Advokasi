d3a374a3eabb2d5fec5b26223a8ae5e60f78ec14
iyank <iyank4@gmail.com>
2012-08-07 09:13:56 +0700

    Just The map, for embedding to AJI Indonesia website

---
+++ themes/advokasi/views/justheader.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+	<title><?php echo $page_title.$site_name; ?></title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700" rel="stylesheet" type="text/css">
+	<?php echo $header_block; ?>
+		<?php
+	// Action::header_scripts - Additional Inline Scripts from Plugins
+	Event::run('ushahidi_action.header_scripts');
+	?>
+</head>
+
+
+<?php
+  // Add a class to the body tag according to the page URI
+  // we're on the home page
+  if (count($uri_segments) == 0)
+  {
+    $body_class = "page-main";
+  }
+  // 1st tier pages
+  elseif (count($uri_segments) == 1)
+  {
+    $body_class = "page-".$uri_segments[0];
+  }
+  // 2nd tier pages... ie "/reports/submit"
+  elseif (count($uri_segments) >= 2)
+  {
+    $body_class = "page-".$uri_segments[0]."-".$uri_segments[1];
+  }
+
+?>
+
+<body id="page" class="<?php echo $body_class; ?>">
+
